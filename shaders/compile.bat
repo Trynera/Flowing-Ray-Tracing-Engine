e2a14007b486bcd5a2f35ed6f8adc32c07adb71b
Trynera <tawilmustafa223@gmail.com>
2024-06-13 20:55:40 +0200

    removed all files for preperation

--- shaders/compile.bat
+++
@@ -1,4 +0,0 @@
-glslc.exe -fshader-stage=vert vert.glsl -o vert.spv
-glslc.exe -fshader-stage=frag frag.glsl -o frag.spv
-
-pause
--- shaders/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-glslc -fshader-stage=vert ../shaders/vert.glsl -o vert.spv
-glslc -fshader-stage=frag ../shaders/frag.glsl -o frag.spv
-
-sleep 5
